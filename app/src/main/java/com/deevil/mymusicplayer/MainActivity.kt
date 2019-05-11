5dfb474e1669f066b8d256f8712b68e780a5d6a3
deevi <fbmYWDJC5W3aBc4>
2019-05-11 04:58:46 +0300

    3

--- app/src/main/java/com/deevil/mymusicplayer/MainActivity.kt
+++ app/src/main/java/com/deevil/mymusicplayer/MainActivity.kt
@@ -8,6 +8,10 @@ import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
 import android.provider.MediaStore
 import androidx.core.net.toUri
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+
+
 
 
 class MainActivity : AppCompatActivity() {
@@ -25,12 +29,32 @@ class MainActivity : AppCompatActivity() {
         })
 
 
+       //rvTrackList
 
-        tracks.add(Track("aaa", "bbb",1, "/faaf.aaa".toUri(), 1))
-        tracks.add(Track("aaa", "bbb",1, "/faaf.aaa".toUri(), 1))
-        tracks.add(Track("aaa", "bbb",1, "/faaf.aaa".toUri(), 1))
-        tracks.add(Track("aaa", "bbb",1, "/faaf.aaa".toUri(), 1))
-        //rvTrackList
+        rvTrackList.setAdapter(ta)
+        val linearLayoutManager = LinearLayoutManager(this)
+        val dividerItemDecoration = DividerItemDecoration(
+            rvTrackList.getContext(),
+            linearLayoutManager.orientation
+        )
+        rvTrackList.setLayoutManager(linearLayoutManager)
+        rvTrackList.addItemDecoration(dividerItemDecoration)
+
+
+
+
+        //R.id.a
+
+
+
+
+
+
+
+        tracks.add(Track("a1", "b1",1, "/faaf.aaa".toUri(), 1))
+        tracks.add(Track("a2", "b2",1, "/faaf.aaa".toUri(), 1))
+        tracks.add(Track("a3", "b3",1, "/faaf.aaa".toUri(), 1))
+        tracks.add(Track("a4", "b4",1, "/faaf.aaa".toUri(), 1))
 
         ta.notifyDataSetChanged()
     }
