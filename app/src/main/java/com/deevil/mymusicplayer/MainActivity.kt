625b6693921beb05c3d1aaf7575f5e9d3f97501c
deevi <fbmYWDJC5W3aBc4>
2019-05-12 13:39:46 +0300

    upd and del

--- app/src/main/java/com/deevil/mymusicplayer/MainActivity.kt
+++ app/src/main/java/com/deevil/mymusicplayer/MainActivity.kt
@@ -2,13 +2,10 @@ package com.deevil.mymusicplayer
 
 import android.Manifest
 import android.annotation.SuppressLint
-import android.content.CursorLoader
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
-import android.provider.MediaStore
-import androidx.core.net.toUri
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import android.content.Intent
@@ -17,23 +14,20 @@ import android.net.Uri
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.documentfile.provider.DocumentFile
 import android.provider.DocumentsContract
 import android.provider.DocumentsContract.Document
 import android.database.Cursor
 import android.media.MediaMetadataRetriever
-import java.io.IOException
 import android.os.Build
 import androidx.annotation.RequiresApi
 import com.google.android.exoplayer2.ExoPlayerFactory
 import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.source.ConcatenatingMediaSource
-import com.google.android.exoplayer2.source.ExtractorMediaSource
-import com.google.android.exoplayer2.source.MediaSource
 import com.google.android.exoplayer2.source.ProgressiveMediaSource
-import com.google.android.exoplayer2.upstream.DataSource
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 import com.google.android.exoplayer2.util.Util
+import kotlinx.android.synthetic.main.activity_main.view.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -41,42 +35,22 @@ class MainActivity : AppCompatActivity() {
     private val PERMISSION_REQUEST_CODE = 1
     private val TAG = "DBG"
 
-    lateinit var ta: TrackAdapter
+
     lateinit var player: SimpleExoPlayer
     lateinit var dataSourceFactory: DefaultDataSourceFactory
-    var tracks: ArrayList<Track> = ArrayList<Track>()
+    lateinit var trackSelector:DefaultTrackSelector
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        player = ExoPlayerFactory.newSimpleInstance(this)
+        trackSelector = DefaultTrackSelector()
+        player = ExoPlayerFactory.newSimpleInstance(this, trackSelector)
         dataSourceFactory = DefaultDataSourceFactory(this, Util.getUserAgent(this, "yourApplicationName"))
 
-
-        ta = TrackAdapter(context = this, tracks = tracks, clickListener = {
-            Log.w("TEST", it.artist)
-            Log.w("TEST", it.uri.toString())
-
-            player.playWhenReady = true
-            //if (player.)
-//
-//            player.prepare(ProgressiveMediaSource.Factory(dataSourceFactory).createMediaSource(it.uri))
-//            player.playWhenReady = true
-        })
-
-
-        //rvTrackList
-
-        rvTrackList.setAdapter(ta)
-        val linearLayoutManager = LinearLayoutManager(this)
-        val dividerItemDecoration = DividerItemDecoration(
-            rvTrackList.getContext(),
-            linearLayoutManager.orientation
-        )
-        rvTrackList.setLayoutManager(linearLayoutManager)
-        rvTrackList.addItemDecoration(dividerItemDecoration)
+        pcv.player = player
+        pcv.controllerHideOnTouch = false
 
 
         button.setOnClickListener {
@@ -97,16 +71,6 @@ class MainActivity : AppCompatActivity() {
 
         }
 
-
-        //R.id.a
-
-
-        tracks.add(Track("a1", "b1", 1, "/faaf.aaa".toUri(), 1))
-        tracks.add(Track("a2", "b2", 1, "/faaf.aaa".toUri(), 1))
-        tracks.add(Track("a3", "b3", 1, "/faaf.aaa".toUri(), 1))
-        tracks.add(Track("a4", "b4", 1, "/faaf.aaa".toUri(), 1))
-
-        ta.notifyDataSetChanged()
     }
 
     @RequiresApi(Build.VERSION_CODES.P)
@@ -117,44 +81,15 @@ class MainActivity : AppCompatActivity() {
 
 
             val treeUri: Uri = data.data
-            var lst = getAllAudioFromTree(treeUri)
+            val lst = getAllAudioFromTree(treeUri)
             if (lst.size > 0) {
-                tracks.clear()
-
-
-
-                for (i in lst) {
-                    val mmr = MediaMetadataRetriever()
-                    mmr.setDataSource(this, i)
-
-                    val title = mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_TITLE)
-                    val artist = mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ARTIST)
-                    val duration = mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION).toLong()
-                    val haveEmbeddedCover = try {
-                        (mmr.embeddedPicture != null)
-                    } catch (e: java.lang.Exception) {
-                        false
-                    }
-                    val havePrimaryCover = try {
-                        (mmr.getPrimaryImage() != null)
-                    } catch (e: java.lang.Exception) {
-                        false
-                    }
-
-                    tracks.add(Track(title, artist, 1, i, duration, haveEmbeddedCover, havePrimaryCover))
-                }
-
 
                 var concatenatedSource = ConcatenatingMediaSource()
-                for (i in tracks) {
-                    concatenatedSource.addMediaSource(ProgressiveMediaSource.Factory(dataSourceFactory).createMediaSource(i.uri))
+                for (i in lst) {
+                    concatenatedSource.addMediaSource(ProgressiveMediaSource.Factory(dataSourceFactory).createMediaSource(i))
                 }
 
                 player.prepare(concatenatedSource)
-                player.playWhenReady = true
-
-                ta.notifyDataSetChanged()
-
             }
 
         }
