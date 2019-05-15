c4b66b6ee77b45d84c65ef4cfbd1e23daa50e596
deevilx <deevilx@gmail.com>
2019-05-15 15:36:46 +0300

    V2

--- app/src/main/java/com/deevil/mymusicplayer/MainActivity.kt
+++ app/src/main/java/com/deevil/mymusicplayer/MainActivity.kt
@@ -17,7 +17,6 @@ import android.provider.DocumentsContract
 import android.provider.DocumentsContract.Document
 import android.database.Cursor
 import android.os.Build
-import android.widget.Toast.makeText
 import androidx.annotation.RequiresApi
 import com.google.android.exoplayer2.*
 import com.google.android.exoplayer2.metadata.Metadata
@@ -26,7 +25,6 @@ import com.google.android.exoplayer2.metadata.id3.TextInformationFrame
 import com.google.android.exoplayer2.source.ConcatenatingMediaSource
 import com.google.android.exoplayer2.source.ProgressiveMediaSource
 import com.google.android.exoplayer2.source.TrackGroupArray
-import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
 import com.google.android.exoplayer2.trackselection.TrackSelectionArray
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 import com.google.android.exoplayer2.util.Util
@@ -34,90 +32,75 @@ import com.google.android.exoplayer2.util.Util
 
 class MainActivity : AppCompatActivity() {
 
-    private val PERMISSION_REQUEST_CODE = 1
+    private val PERMISSION_REQUEST_CODE = 9998
+    private val DIRECTORY_REQUEST_CODE = 9999
     private val TAG = "DBG"
 
 
     lateinit var player: ExoPlayer
     lateinit var dataSourceFactory: DefaultDataSourceFactory
-    lateinit var trackSelector: DefaultTrackSelector
 
 
     @SuppressLint("WrongConstant")
     override fun onCreate(savedInstanceState: Bundle?) {
-        Log.w(TAG, "onCreate")
+        Log.i(TAG, "onCreate")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        trackSelector = DefaultTrackSelector()
-        //var pc:PlayerControlView
-
-        player = ExoPlayerFactory.newSimpleInstance(this, trackSelector)
-        //player.debug
+        // INIT PLAYER
+        player = ExoPlayerFactory.newSimpleInstance(this)
         dataSourceFactory = DefaultDataSourceFactory(this, Util.getUserAgent(this, "yourApplicationName"))
-
-
-        //pc.player = player
         pcv.player = player
         pcv.controllerHideOnTouch = false
 
-        btn_settings.setOnClickListener {
 
-            if (ContextCompat.checkSelfPermission(
-                    this,
-                    Manifest.permission.READ_EXTERNAL_STORAGE
-                ) != PackageManager.PERMISSION_GRANTED
-            ) {
-                ActivityCompat.requestPermissions(
-                    this,
-                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
-                    PERMISSION_REQUEST_CODE
-                )
-            } else {
-                ChooseDirectory()
-            }
-
-        }
-
+        // SWIPE
         pcv.setOnTouchListener(object : SwipeListener(this){
 
             override fun onSwipeTop() {
-                Log.w(TAG, "onSwipeTop")
+                Log.i(TAG, "onSwipeTop")
             }
 
             override fun onSwipeRight() {
-                Log.w(TAG, "onSwipeRight")
+                Log.i(TAG, "onSwipeRight")
                 if (player.hasPrevious()) player.previous()
             }
 
             override fun onSwipeLeft() {
-                Log.w(TAG, "onSwipeLeft")
+                Log.i(TAG, "onSwipeLeft")
                 if (player.hasNext()) player.next()
             }
 
             override fun onSwipeBottom() {
-                Log.w(TAG, "onSwipeBottom")
-               // hom()
+                Log.i(TAG, "onSwipeBottom")
+                // Hide app
+                val startMain = Intent(Intent.ACTION_MAIN)
+                startMain.addCategory(Intent.CATEGORY_HOME)
+                startMain.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                startActivity(startMain)
             }
 
         })
 
 
+        // BUTTON CLICKS
+        btn_settings.setOnClickListener {selectDir()}
+        btn_add.setOnClickListener {selectDir()}
         btn_repeat.setOnClickListener {
             btn_repeat.isSelected = !btn_repeat.isSelected
             player.repeatMode = if (btn_repeat.isSelected) 2 else 0
-
         }
-
         btn_shuffle.setOnClickListener {
             btn_shuffle.isSelected = !btn_shuffle.isSelected
             player.shuffleModeEnabled = btn_shuffle.isSelected
 
         }
+
+        // PLAYER TAGS (Title, Artist)
         player.addListener(object : Player.EventListener {
             override fun onLoadingChanged(isLoading: Boolean) {
                 if (!isLoading) {
-                    Log.w(TAG, "aaaa")
+                    Log.i(TAG, "onLoadingChanged")
                 }
             }
 
@@ -142,13 +125,32 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    @RequiresApi(Build.VERSION_CODES.P)
-    @SuppressLint("MissingSuperCall")
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if (requestCode == 9999 && resultCode == RESULT_OK && data != null && data.data != null) {
-            Log.i(TAG, "Result URI " + data.data)
 
-            //if (data.data != null) return
+    /**
+     * Check, ask permissions and choose dir
+     */
+    private fun selectDir() {
+
+        // Check permissions
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), PERMISSION_REQUEST_CODE)
+        } else {
+            val i = Intent(Intent.ACTION_OPEN_DOCUMENT_TREE)
+
+            i.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+            i.addFlags(Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION)
+            i.addCategory(Intent.CATEGORY_DEFAULT)
+            //i.setType("audio/*");
+            startActivityForResult(Intent.createChooser(i, "Choose directory"), DIRECTORY_REQUEST_CODE)
+        }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == DIRECTORY_REQUEST_CODE && resultCode == RESULT_OK && data != null && data.data != null) {
+            Log.i(TAG, "Result URI " + data.data)
 
             val treeUri: Uri = data.data
             val lst = getAllAudioFromTree(treeUri)
@@ -156,17 +158,16 @@ class MainActivity : AppCompatActivity() {
 
                 var concatenatedSource = ConcatenatingMediaSource()
                 for (i in lst) {
-                    concatenatedSource.addMediaSource(
-                        ProgressiveMediaSource.Factory(dataSourceFactory).createMediaSource(
-                            i
-                        )
-                    )
+                    concatenatedSource.addMediaSource(ProgressiveMediaSource.Factory(dataSourceFactory).createMediaSource(i))
                 }
 
                 player.prepare(concatenatedSource)
+            } else {
+                Toast.makeText(this, "В выбранной директории нет аудио файлов", Toast.LENGTH_LONG).show()
             }
 
         }
+
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
@@ -175,23 +176,14 @@ class MainActivity : AppCompatActivity() {
         if (requestCode == PERMISSION_REQUEST_CODE) {
             if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 Log.i(TAG, "Permission granted!")
-                ChooseDirectory()
+                selectDir()
             } else {
                 Log.i(TAG, "Permission denied")
-                Toast.makeText(this, "Not permission to read files", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "Нет прав на чтение файловой системы", Toast.LENGTH_SHORT).show()
             }
         }
     }
 
-    fun ChooseDirectory() {
-        val i = Intent(Intent.ACTION_OPEN_DOCUMENT_TREE)
-
-        i.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
-        i.addFlags(Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION)
-        i.addCategory(Intent.CATEGORY_DEFAULT)
-        //i.setType("audio/*");
-        startActivityForResult(Intent.createChooser(i, "Choose directory"), 9999)
-    }
 
     private fun getAllAudioFromTree(treeUri: Uri, inpParentDocumentId: String? = null): ArrayList<Uri> {
 
@@ -243,31 +235,31 @@ class MainActivity : AppCompatActivity() {
 
 
     override fun onStart() {
-        Log.w(TAG, "onStart")
+        Log.i(TAG, "onStart")
         super.onStart()
     }
     override fun onStop() {
-        Log.w(TAG, "onStop")
+        Log.i(TAG, "onStop")
         super.onStop()
     }
 
     override fun onRestart() {
-        Log.w(TAG, "onRestart")
+        Log.i(TAG, "onRestart")
         super.onRestart()
     }
 
     override fun onResume() {
-        Log.w(TAG, "onResume")
+        Log.i(TAG, "onResume")
         super.onResume()
     }
 
     override fun onDestroy() {
-        Log.w(TAG, "onDestroy")
+        Log.i(TAG, "onDestroy")
         super.onDestroy()
     }
 
     override fun onPause() {
-        Log.w(TAG, "onPause")
+        Log.i(TAG, "onPause")
         super.onPause()
 
     }
